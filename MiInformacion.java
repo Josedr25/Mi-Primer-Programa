e9f87bebd189fda1f6eb6ea6d9ff2fe047d500ca
Josedr25 <159076150+Josedr25@users.noreply.github.com>
2024-03-17 20:52:11 -0300

    Add files via upload

---
+++ MiInformacion.java
@@ -0,0 +1,10 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        
+        System.out.println("Mi nombre es Jose");
+
+        System.out.println("Tengo 21");
+
+        System.out.println("Mi ciudad es Lara, VE");
+    }
+}
